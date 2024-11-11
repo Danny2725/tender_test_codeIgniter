bba53258221a2c048b567d2382f6abfd30e735b0
Danny <tranquan.ttdt.usec@gmail.com>
2024-11-11 14:25:53 +0700

    update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'codeigniter4/appstarter',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '7ae740f7a13a3a424810968113336b96464ba12d',
+        'reference' => 'a03198d284fa578cc08211c9c9fc4ed927226460',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'codeigniter4/appstarter' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '7ae740f7a13a3a424810968113336b96464ba12d',
+            'reference' => 'a03198d284fa578cc08211c9c9fc4ed927226460',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
